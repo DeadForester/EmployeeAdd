ce6376e7dcd47575236ce1a38fbe9d69decc7288
Mathew Pahnyn <mattew1997@yandex.ru>
2024-11-09 19:03:22 +0300

    1

---
+++ src/main/java/com/spring/employees/dao/EmployeeDAO.java
@@ -0,0 +1,15 @@
+package com.spring.employees.dao;
+
+import com.spring.employees.entity.Employee;
+
+import java.util.List;
+
+public interface EmployeeDAO {
+    List<Employee> getAllEmployees();
+
+    void saveEmployee(Employee employee);
+
+    Employee getEmployee(int id);
+
+    void deleteEmployee(int id);
+}
---
+++ src/main/java/com/spring/employees/service/EmployeeService.java
@@ -0,0 +1,16 @@
+package com.spring.employees.service;
+
+import com.spring.employees.entity.Employee;
+
+import java.util.List;
+
+public interface EmployeeService {
+
+    List<Employee> getAllEmployees();
+
+    void saveEmployee(Employee employee);
+
+    Employee getEmployee(int id);
+
+    void deleteEmployee(int id);
+}
